ad9018c67bbd83bea50ebd56d80fada137a9d86b
Maximilian Mehnert <maximilian.mehnert@gmx.de>
2013-01-31 11:59:06 +0100

    add debian server configuration

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,6 @@ default['openmeetings_mysql']['database'] = "openmeetings"
 default['openmeetings_mysql']['username'] = "openmeetings"
 default['openmeetings_mysql']['password'] = "password"
 
+node.set_unless['mysql']['server_debian_password'] = "testpassword"
+node.set_unless['mysql']['server_root_password'] = "testpassword"
+node.set_unless['mysql']['server_repl_password'] = "testpassword"
