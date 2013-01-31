4a205875d83fdc631d1055f6f4a94026e516ad3d
Maximilian Mehnert <maximilian.mehnert@gmx.de>
2013-01-31 13:05:32 +0100

    force mysql bind to localhost

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,7 @@
 default['openmeetings_mysql']['database'] = "openmeetings"
 default['openmeetings_mysql']['username'] = "openmeetings"
 default['openmeetings_mysql']['password'] = "password"
+default['mysql']['bind_address'] = "localhost"
 
 node.set_unless['mysql']['server_debian_password'] = "testpassword"
 node.set_unless['mysql']['server_root_password'] = "testpassword"
